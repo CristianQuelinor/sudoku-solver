6d2c5d9f065ac625069e33709ffa6aed34b080ce
trinquier <nicolas.trinquier@telecom-paristech.fr>
2014-04-09 15:22:07 +0200

    Improvement of the verification method

--- src/fonctions.c
+++ src/fonctions.c
@@ -2,67 +2,70 @@
 #include <stdlib.h>
 #include "fonctions.h"
 
-// Returns true if k is in the row, else false
-bool isNotInRow(int k, int grid[9][9], int row) {
-	for (int column = 0 ; column < 9 ; column++) {
-		if (grid[row][column] == k)
-			return false;
-	}
-
-	return true;
-}
-
-// Returns true if k is in the column, else false
-bool isNotInColumn(int k, int grid[9][9], int column) {
-	for (int row = 0 ; row < 9 ; row++) {
-		if (grid[row][column] == k)
-			return false;
-	}
-
-	return true;
-}
-
-// Returns true if k is in the bloc whose left top corner is (row, column), else false
-bool isNotInBloc(int k, int grid[9][9], int _row, int _column) {
-	_row = _row-(_row%3);
-	_column = _column-(_column%3);
-	for (int row = _row ; row < _row + 3 ; row++) {
-		for (int column = _column ; column < _column + 3 ; column++) {
-			if (grid[row][column] == k)
-				return false;
-		}
-	}
-
-	return true;
-}
-
 // Assuming that the cursor is in "index"
 // Returns true if the grid is valid, else false
-bool isValid(int grid[9][9], int index) {
+bool isValid(int grid[9][9], bool isInRow[9][9], bool isInColumn[9][9], bool isInBloc[9][9], int index) {
 	if (index == 81) // It means we have finished the grid
 		return true;
 
 	int row = index/9, column = index%9;
 
 	if (grid[row][column] != 0) // If the grid's not empty, it means we have to go no the next index
-		return isValid(grid, index+1);
+		return isValid(grid, isInRow, isInColumn, isInBloc, index+1);
 
-	for (int k = 1; k <= 9; k++) { // Test of every possible value
-		if (isNotInRow(k, grid, row) && isNotInColumn(k, grid, column) && isNotInBloc(k, grid, row, column)) {
-			grid[row][column] = k; // Let's assume the correct value is k
+	for (int k = 0; k < 9; k++) { // Test of every possible value
+		if (!isInRow[row][k] && !isInColumn[column][k] && !isInBloc[3*(row/3)+(column/3)][k]) {
+			// Let's assume the correct value is k (well, k + 1 actually)
+			isInRow[row][k] = true;
+			isInColumn[column][k] = true;
+			isInBloc[3*(row/3)+(column/3)][k] = true;
 
-			if (isValid(grid, index+1))
+			if (isValid(grid, isInRow, isInColumn, isInBloc, index+1)) {
+				grid[row][column] = k + 1; // We write in the right box
 				return true;
+			}
+
+			// Erasing of the old value in case it was wrong
+			isInRow[row][k] = false;
+			isInColumn[column][k] = false;
+			isInBloc[3*(row/3)+(column/3)][k] = false;
 		}
 	}
 
-	grid[row][column] = 0; // Erasing of the old value in case it was wrong
 	return false;
 }
 
-// Solve the grid
+// Solve the grid (backtracking algorithm)
 void solve(int grid[9][9]) {
-	isValid(grid, 0);
+	bool isInRow[9][9];
+	bool isInColumn[9][9];
+	bool isInBloc[9][9];
+
+	// Init
+	for (int row = 0 ; row < 9 ; row++) {
+		for (int column = 0 ; column < 9 ; column++) {
+			isInRow[row][column] = false;
+			isInColumn[row][column] = false;
+			isInBloc[row][column] = false;
+		}
+	}
+
+	// Init, writting true where it's supposed to be
+	int t;
+	for (int row = 0 ; row < 9 ; row++) {
+		for (int column = 0 ; column < 9 ; column++) {
+			int t = grid[row][column];
+
+			if (t != 0) { // If the box's not empty - 1;
+				t--;
+				isInRow[row][t] = true;
+				isInColumn[column][t] = true;
+				isInBloc[3*(row/3)+(column/3)][t] = true;
+			}
+		}
+	}
+
+	isValid(grid, isInRow, isInColumn, isInBloc, 0);
 }
 
 // Load the grid
--- src/fonctions.h
+++ src/fonctions.h
@@ -1,10 +1,7 @@
 #ifndef FONCTIONS
 #define FONCTIONS
 
-bool isNotInRow(int k, int grid[9][9], int row);
-bool isNotInColumn(int k, int grid[9][9], int column);
-bool isNotInBloc(int k, int grid[9][9], int _row, int _column);
-bool isValid(int grid[9][9], int position);
+bool isValid(int grid[9][9], bool isInRow[9][9], bool isInColumn[9][9], bool isInBloc[9][9], int position);
 void solve(int grid[9][9]);
 bool loadGrid(int grid[9][9]);
 void print(int grid[9][9]);
