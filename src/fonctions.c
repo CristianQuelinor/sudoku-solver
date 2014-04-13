bc37a56549989511596a1a9e8aa72080cf96f6df
trinquier <nicolas.trinquier@telecom-paristech.fr>
2014-04-13 22:50:14 +0200

    Improvement of the algorithm: non-linear exploration of the grid

--- src/fonctions.c
+++ src/fonctions.c
@@ -1,75 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "fonctions.h"
+#include "list.h"
 
-// Assuming that the cursor is in "index"
-// Returns true if the grid is valid, else false
-bool isValid(int grid[9][9], bool isInRow[9][9], bool isInColumn[9][9], bool isInBloc[9][9], int index) {
-	if (index == 81) // It means we have finished the grid
-		return true;
+// STATIC FUNCTIONS
+static int isValid(int grid[9][9], LIST* toExamine);
+static int count(int grid[9][9], int i, int j);
 
-	int row = index/9, column = index%9;
+// GLOBAL VARIABLES
+int isInRow[9][9];
+int isInColumn[9][9];
+int isInBloc[9][9];
 
-	if (grid[row][column] != 0) // If the grid's not empty, it means we have to go no the next index
-		return isValid(grid, isInRow, isInColumn, isInBloc, index+1);
+// Assuming that the cursor is in "toExamine"
+// Returns 1 if the grid is valid, else 0
+static int isValid(int grid[9][9], LIST* toExamine) {	
+	if (toExamine == NULL) // It means we have finished the grid
+		return 1;
 
-	for (int k = 0; k < 9; k++) { // Test of every possible value
+	int row = toExamine->i, column = toExamine->j, k;
+
+	if (grid[row][column] != 0) // If the grid's not empty, it means we have to go no the next case
+		return isValid(grid, toExamine->next);
+
+	for (k = 0; k < 9; k++) { // Test of every possible value
 		if (!isInRow[row][k] && !isInColumn[column][k] && !isInBloc[3*(row/3)+(column/3)][k]) {
 			// Let's assume the correct value is k (well, k + 1 actually)
-			isInRow[row][k] = true;
-			isInColumn[column][k] = true;
-			isInBloc[3*(row/3)+(column/3)][k] = true;
+			isInRow[row][k] = 1;
+			isInColumn[column][k] = 1;
+			isInBloc[3*(row/3)+(column/3)][k] = 1;
 
-			if (isValid(grid, isInRow, isInColumn, isInBloc, index+1)) {
+			if (isValid(grid, toExamine->next)) {
 				grid[row][column] = k + 1; // We write in the right box
-				return true;
+				return 1;
 			}
 
 			// Erasing of the old value in case it was wrong
-			isInRow[row][k] = false;
-			isInColumn[column][k] = false;
-			isInBloc[3*(row/3)+(column/3)][k] = false;
+			isInRow[row][k] = 0;
+			isInColumn[column][k] = 0;
+			isInBloc[3*(row/3)+(column/3)][k] = 0;
 		}
 	}
 
-	return false;
+	return 0;
+}
+
+// Returns the number of possible values in (i, j)
+static int count(int grid[9][9], int row, int column) {
+	int ret = 0, k;
+	
+	for (k = 0 ; k < 9 ; k++)
+		if (!isInRow[row][k] && !isInColumn[column][k] && !isInBloc[3*(row/3)+(column/3)][k])
+			k++;
+	
+	return ret;
 }
 
 // Solve the grid (backtracking algorithm)
 void solve(int grid[9][9]) {
-	bool isInRow[9][9];
-	bool isInColumn[9][9];
-	bool isInBloc[9][9];
+	// Initialization of the "path" to follow to test solutions
+	int row, column;
+	LIST* toExamine = NULL;
+	
+	for (row = 0 ; row < 9 ; row++)
+		for (column = 0 ; column < 9 ; column++)
+			if (grid[row][column] == 0)
+				pushFront(&toExamine, row, column, count(grid, row, column));
 
-	// Init
-	for (int row = 0 ; row < 9 ; row++) {
-		for (int column = 0 ; column < 9 ; column++) {
-			isInRow[row][column] = false;
-			isInColumn[row][column] = false;
-			isInBloc[row][column] = false;
+	toExamine = insertionSort(toExamine);
+	
+	// Init of arrays
+	for (row = 0 ; row < 9 ; row++) {
+		for (column = 0 ; column < 9 ; column++) {
+			isInRow[row][column] = 0;
+			isInColumn[row][column] = 0;
+			isInBloc[row][column] = 0;
 		}
 	}
 
-	// Init
+	// Init of arrays
 	int t;
-	for (int row = 0 ; row < 9 ; row++) {
-		for (int column = 0 ; column < 9 ; column++) {
-			int t = grid[row][column];
+	for (row = 0 ; row < 9 ; row++) {
+		for (column = 0 ; column < 9 ; column++) {
+			t = grid[row][column];
 
-			if (t != 0) { // If the box's not empty; // isInRow[row][t] = true <==> t+1 is in the row
+			if (t != 0) { // If the box's not empty; // isInRow[row][t] = 1 <==> t+1 is in the row
 				t--;
-				isInRow[row][t] = true;
-				isInColumn[column][t] = true;
-				isInBloc[3*(row/3)+(column/3)][t] = true;
+				isInRow[row][t] = 1;
+				isInColumn[column][t] = 1;
+				isInBloc[3*(row/3)+(column/3)][t] = 1;
 			}
 		}
 	}
 
-	isValid(grid, isInRow, isInColumn, isInBloc, 0);
+	isValid(grid, toExamine);
+	
+	deleteList(&toExamine);
 }
 
 // Load the grid
-bool loadGrid(int grid[9][9]) {
+int loadGrid(int grid[9][9]) {
 	int row = 0, column = 0;
 
 	FILE *file;
@@ -77,7 +107,7 @@ bool loadGrid(int grid[9][9]) {
 
 	if (file == NULL) {
 		printf("Error: Couln't open grid");
-		return false;
+		return 0;
 	}
 
 	char c;
@@ -101,13 +131,14 @@ bool loadGrid(int grid[9][9]) {
 	}
 
 	fclose (file);
-	return true;
+	return 1;
 }
 
 // Print the grid
 void print(int grid[9][9]) {
-	for (int row = 0 ; row < 9 ; row++) {
-		for (int column = 0 ; column < 9 ; column++) {
+	int row, column;
+	for (row = 0 ; row < 9 ; row++) {
+		for (column = 0 ; column < 9 ; column++) {
 			if (column == 3 || column == 6)
 				printf("|");
 
--- src/fonctions.h
+++ src/fonctions.h
@@ -1,9 +1,8 @@
-#ifndef FONCTIONS
-#define FONCTIONS
+#ifndef FONCTIONS_H
+#define FONCTIONS_H
 
-bool isValid(int grid[9][9], bool isInRow[9][9], bool isInColumn[9][9], bool isInBloc[9][9], int position);
 void solve(int grid[9][9]);
-bool loadGrid(int grid[9][9]);
+int loadGrid(int grid[9][9]);
 void print(int grid[9][9]);
 
 #endif
